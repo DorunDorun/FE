a06c621608296b7ce97551ee4aa2b75fdc99390e
cheongu <kangsun0528@gmail.com>
2023-02-14 17:14:38 +0900

    google login 추가중

--- src/shared/OAuth.js
+++ src/shared/OAuth.js
@@ -1,4 +1,6 @@
 const CLIENT_ID = "	68b0ace8b9adf8d9b54389e448ad653d";
-const REDIRECT_URI = "http://localhost:3000/oauth/callback/kakao";
+const REDIRECT_URI = "http://localhost:3000/home";
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+export const GOOGLE_AUTH_URL = ``;
