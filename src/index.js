7e526b8b766db496086ae4d1fd558d4d854b5d34
cheongu <kangsun0528@gmail.com>
2023-02-16 22:14:50 +0900

    쿠키 삭제

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./css/index.css";
 import App from "./App";
-import { CookieProvider } from "react-cookie";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <CookieProvider>
-    <App />
-  </CookieProvider>
-);
+root.render(<App />);
