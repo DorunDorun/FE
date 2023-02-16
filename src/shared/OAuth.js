7ba01126835aaebd044b531423961df496a86335
cheongu <kangsun0528@gmail.com>
2023-02-16 22:16:17 +0900

    카카오 구글 네이버 분리

--- src/shared/OAuth.js
+++ src/shared/OAuth.js
@@ -1,6 +1,20 @@
-const CLIENT_ID = "	68b0ace8b9adf8d9b54389e448ad653d";
-const REDIRECT_URI = "http://localhost:3000/home";
+// REST API
+const KAKAO_CLIENT_ID = "68b0ace8b9adf8d9b54389e448ad653d";
+const GOOGLE_CLIENT_ID =
+  "144351566914-uljifaoirfhsdh15dpic7t7kacat4bm6.apps.googleusercontent.com";
+const NAVER_CLIENT_ID = "EqbwugkRagVJo1L1FnGb";
 
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+// 리다이렉트 주소
+const KAKAO_REDIRECT_URI = "https://dorundorun.shop/login/oauth2/code/kakao";
+const GOOGLE_REDIRECT_URI = "https://dorundorun.shop/login/oauth2/code/google";
+const NAVER_REDIRECT_URI = "https://dorundorun.shop/login/oauth2/code/naver";
 
-export const GOOGLE_AUTH_URL = ``;
+// export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+// export const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_REDIRECT_URI}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email`;
+// export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=STATE_STRING&redirect_uri=${NAVER_REDIRECT_URI}`;
+
+export const KAKAO_AUTH_URL = `https://dorundorun.shop/oauth2/authorization/kakao?redirect_uri=${KAKAO_REDIRECT_URI}`;
+
+export const GOOGLE_AUTH_URL = `https://dorundorun.shop/oauth2/authorization/google?redirect_uri=${GOOGLE_REDIRECT_URI}`;
+
+export const NAVER_AUTH_URL = `https://dorundorun.shop/oauth2/authorization/naver?redirect_uri=${NAVER_REDIRECT_URI}`;
