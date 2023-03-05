ee06a1a17a6468d9980996d46b0160bd4c976267
IOTrue <kjoonc.dev@gmail.com>
2023-03-05 21:23:52 +0900

    이미지 절대경로 추가

--- src/Components/lists/ButtonImageList.js
+++ src/Components/lists/ButtonImageList.js
@@ -1,17 +1,17 @@
 const ButtonImageList = {
     video: {
       name: "video",
-      onSmall: "./asset/images/button/video-on-s.PNG",
-      offSmall: "./asset/images/button/video-off-s.PNG",
-      onMedium: "./asset/images/button/video-on-m.PNG",
-      offMedium: "./asset/images/button/video-off-m.PNG",
+      onSmall: process.env.PUBLIC_URL+"./asset/images/button/video-on-s.PNG",
+      offSmall: process.env.PUBLIC_URL+"./asset/images/button/video-off-s.PNG",
+      onMedium: process.env.PUBLIC_URL+"./asset/images/button/video-on-m.PNG",
+      offMedium: process.env.PUBLIC_URL+"./asset/images/button/video-off-m.PNG",
     },
     audio: {
         name: "audio",
-        onSmall: "./asset/images/button/audio-on-s.PNG",
-        offSmall: "./asset/images/button/audio-off-s.PNG",
-        onMedium: "./asset/images/button/audio-on-m.PNG",
-        offMedium: "./asset/images/button/audio-off-m.PNG",
+        onSmall: process.env.PUBLIC_URL+"./asset/images/button/audio-on-s.PNG",
+        offSmall: process.env.PUBLIC_URL+"./asset/images/button/audio-off-s.PNG",
+        onMedium: process.env.PUBLIC_URL+"./asset/images/button/audio-on-m.PNG",
+        offMedium: process.env.PUBLIC_URL+"./asset/images/button/audio-off-m.PNG",
     },
   };
   
--- src/Components/lists/CategoryImageList.js
+++ src/Components/lists/CategoryImageList.js
@@ -1,40 +1,38 @@
 const CategoryImageList = {
   STUDY: {
     name: "STUDY",
-    smallImageUrl: "./asset/images/category/categoryImage-small-study.PNG",
-    backgroundImageUrl: "./asset/images/category/categoryImage-back-study.PNG",
+    smallImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-small-study.PNG",
+    backgroundImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-back-study.PNG",
   },
   SOCIAL: {
     name: "SOCIAL",
-    smallImageUrl: "./asset/images/category/categoryImage-small-social.PNG",
-    backgroundImageUrl: "./asset/images/category/categoryImage-back-social.PNG",
+    smallImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-small-social.PNG",
+    backgroundImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-back-social.PNG",
   },
   HOBBY: {
     name: "HOBBY",
-    smallImageUrl: "./asset/images/category/categoryImage-small-hobby.PNG",
-    backgroundImageUrl: "./asset/images/category/categoryImage-back-hobby.PNG",
+    smallImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-small-hobby.PNG",
+    backgroundImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-back-hobby.PNG",
   },
   WORKOUT: {
     name: "WORKOUT",
-    smallImageUrl: "./asset/images/category/categoryImage-small-workout.PNG",
-    backgroundImageUrl:
-      "./asset/images/category/categoryImage-back-workout.PNG",
+    smallImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-small-workout.PNG",
+    backgroundImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-back-workout.PNG",
   },
   JOBS: {
     name: "JOBS",
-    smallImageUrl: "./asset/images/category/categoryImage-small-jobs.PNG",
-    backgroundImageUrl: "./asset/images/category/categoryImage-back-jobs.PNG",
+    smallImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-small-jobs.PNG",
+    backgroundImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-back-jobs.PNG",
   },
   INVESTMENT: {
     name: "INVESTMENT",
-    smallImageUrl: "./asset/images/category/categoryImage-small-investment.PNG",
-    backgroundImageUrl:
-      "./asset/images/category/categoryImage-back-investment.PNG",
+    smallImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-small-investment.PNG",
+    backgroundImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-back-investment.PNG",
   },
   ETC: {
     name: "ETC",
-    smallImageUrl: "./asset/images/category/categoryImage-small-etc.PNG",
-    backgroundImageUrl: "./asset/images/category/categoryImage-back-etc.PNG",
+    smallImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-small-etc.PNG",
+    backgroundImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-back-etc.PNG",
   },
 };
 
