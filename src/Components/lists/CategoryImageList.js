b2847340ca9edf41f0e7525e85d17b031de3cea9
IOTrue <kjoonc.dev@gmail.com>
2023-03-05 21:27:55 +0900

    이미지 절대경로 작업중

--- src/Components/lists/CategoryImageList.js
+++ src/Components/lists/CategoryImageList.js
@@ -1,38 +1,38 @@
 const CategoryImageList = {
   STUDY: {
     name: "STUDY",
-    smallImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-small-study.png",
-    backgroundImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-back-study.png",
+    smallImageUrl: "./asset/images/category/categoryImage-small-study.png",
+    backgroundImageUrl: "./asset/images/category/categoryImage-back-study.png",
   },
   SOCIAL: {
     name: "SOCIAL",
-    smallImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-small-social.png",
-    backgroundImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-back-social.png",
+    smallImageUrl: "./asset/images/category/categoryImage-small-social.png",
+    backgroundImageUrl: "./asset/images/category/categoryImage-back-social.png",
   },
   HOBBY: {
     name: "HOBBY",
-    smallImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-small-hobby.png",
-    backgroundImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-back-hobby.png",
+    smallImageUrl: "./asset/images/category/categoryImage-small-hobby.png",
+    backgroundImageUrl: "./asset/images/category/categoryImage-back-hobby.png",
   },
   WORKOUT: {
     name: "WORKOUT",
-    smallImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-small-workout.png",
-    backgroundImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-back-workout.png",
+    smallImageUrl: "./asset/images/category/categoryImage-small-workout.png",
+    backgroundImageUrl: "./asset/images/category/categoryImage-back-workout.png",
   },
   JOBS: {
     name: "JOBS",
-    smallImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-small-jobs.png",
-    backgroundImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-back-jobs.png",
+    smallImageUrl: "./asset/images/category/categoryImage-small-jobs.png",
+    backgroundImageUrl: "./asset/images/category/categoryImage-back-jobs.png",
   },
   INVESTMENT: {
     name: "INVESTMENT",
-    smallImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-small-investment.png",
-    backgroundImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-back-investment.png",
+    smallImageUrl: "./asset/images/category/categoryImage-small-investment.png",
+    backgroundImageUrl: "./asset/images/category/categoryImage-back-investment.png",
   },
   ETC: {
     name: "ETC",
-    smallImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-small-etc.png",
-    backgroundImageUrl: process.env.PUBLIC_URL+"./asset/images/category/categoryImage-back-etc.png",
+    smallImageUrl: "./asset/images/category/categoryImage-small-etc.png",
+    backgroundImageUrl: "./asset/images/category/categoryImage-back-etc.png",
   },
 };
 
